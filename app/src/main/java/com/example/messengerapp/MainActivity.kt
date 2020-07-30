49fd7681ba3798eef72e05a783b1a73b18f75e97
Sameera Dissanayaka <sameerad177@gmail.com>
2020-07-30 22:40:22 +0530

    Add multidex enabled dependencies to gradle

--- app/src/main/java/com/example/messengerapp/MainActivity.kt
+++ app/src/main/java/com/example/messengerapp/MainActivity.kt
@@ -7,7 +7,13 @@ import androidx.appcompat.app.AppCompatActivity
 import android.view.Menu
 import android.view.MenuItem
 import android.widget.Toolbar
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
 import androidx.viewpager.widget.ViewPager
+import com.example.messengerapp.Fragments.ChatFragment
+import com.example.messengerapp.Fragments.SearchFragment
+import com.example.messengerapp.Fragments.SettingsFragment
 import com.google.android.material.tabs.TabLayout
 
 class MainActivity : AppCompatActivity() {
@@ -24,6 +30,14 @@ class MainActivity : AppCompatActivity() {
         val tabLayout:TabLayout=findViewById(R.id.tab_layout)
         val viewPager:ViewPager=findViewById(R.id.view_pager)
 
+        val viewPagerAdapter =ViewPagerAdapter(supportFragmentManager)
+
+        viewPagerAdapter.addFragments(ChatFragment(),"Chats")
+        viewPagerAdapter.addFragments(SearchFragment(),"Search")
+        viewPagerAdapter.addFragments(SettingsFragment(),"Settings")
+
+        viewPager.adapter=viewPagerAdapter
+        tabLayout.setupWithViewPager(viewPager)
 
     }
 
@@ -42,4 +56,35 @@ class MainActivity : AppCompatActivity() {
             else -> super.onOptionsItemSelected(item)
         }
     }
+
+    internal class ViewPagerAdapter(fragmentManager: FragmentManager):
+        FragmentPagerAdapter(fragmentManager)
+    {
+        private val fragments:ArrayList<Fragment>
+        private val titles:ArrayList<String>
+        init {
+            fragments= ArrayList<Fragment>()
+            titles=ArrayList<String>()
+        }
+
+
+        override fun getItem(position: Int): Fragment {
+            return fragments[position]
+        }
+
+        override fun getCount(): Int {
+            return fragments.size
+        }
+fun addFragments(fragment: Fragment,title:String)
+{
+    fragments.add(fragment)
+    titles.add(title)
+
+}
+
+        override fun getPageTitle(i: Int): CharSequence? {
+            return titles[i]
+        }
+    }
+
 }
