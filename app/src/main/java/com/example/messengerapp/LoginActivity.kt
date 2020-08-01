8d989c1c5bf1e46d75263b4dc1f9665901f24cd5
Sameera Dissanayaka <sameerad177@gmail.com>
2020-08-01 21:05:14 +0530

    Back button working for navigation

--- app/src/main/java/com/example/messengerapp/LoginActivity.kt
+++ app/src/main/java/com/example/messengerapp/LoginActivity.kt
@@ -1,5 +1,6 @@
 package com.example.messengerapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,17 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        val toolbar:androidx.appcompat.widget.Toolbar=findViewById(R.id.toolbar_login)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title="Login"
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        toolbar.setNavigationOnClickListener {
+            val intent= Intent(this@LoginActivity,WelcomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
+
+
     }
 }
--- app/src/main/java/com/example/messengerapp/RegisterActivity.kt
+++ app/src/main/java/com/example/messengerapp/RegisterActivity.kt
@@ -1,5 +1,6 @@
 package com.example.messengerapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
@@ -7,5 +8,15 @@ class RegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
+
+        val toolbar:androidx.appcompat.widget.Toolbar=findViewById(R.id.toolbar_register)
+        setSupportActionBar(toolbar)
+        supportActionBar!!.title="Register"
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+        toolbar.setNavigationOnClickListener {
+            val intent= Intent(this@RegisterActivity,WelcomeActivity::class.java)
+            startActivity(intent)
+            finish()
+        }
     }
 }
