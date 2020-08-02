d0da698a682dafd0f859d78b8d887153c4a8855e
Sameera Dissanayaka <sameerad177@gmail.com>
2020-08-02 21:27:12 +0530

    Added intent to Register user and login user

--- app/src/main/java/com/example/messengerapp/RegisterActivity.kt
+++ app/src/main/java/com/example/messengerapp/RegisterActivity.kt
@@ -3,20 +3,94 @@ package com.example.messengerapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.database.DatabaseReference
+import com.google.firebase.database.FirebaseDatabase
+import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
+
+    private lateinit var mAuth: FirebaseAuth
+    private lateinit var refUsers: DatabaseReference
+    private var firebaseUserID: String = ""
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
-        val toolbar:androidx.appcompat.widget.Toolbar=findViewById(R.id.toolbar_register)
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_register)
         setSupportActionBar(toolbar)
-        supportActionBar!!.title="Register"
+        supportActionBar!!.title = "Register"
         supportActionBar!!.setDisplayHomeAsUpEnabled(true)
         toolbar.setNavigationOnClickListener {
-            val intent= Intent(this@RegisterActivity,WelcomeActivity::class.java)
+            val intent = Intent(this@RegisterActivity, WelcomeActivity::class.java)
             startActivity(intent)
             finish()
         }
+
+        mAuth = FirebaseAuth.getInstance()
+
+        register_btn.setOnClickListener {
+            registerUser()
+        }
+
+    }
+
+    private fun registerUser() {
+        val username: String = username_register.text.toString()
+        val email: String = email_register.text.toString()
+        val password: String = password_register.text.toString()
+
+        if (username == "") {
+            Toast.makeText(this@RegisterActivity, "Pelase Enter Username", Toast.LENGTH_LONG).show()
+
+        } else if (email == "") {
+            Toast.makeText(this@RegisterActivity, "Pelase Enter Email", Toast.LENGTH_LONG).show()
+
+        } else if (password == "") {
+            Toast.makeText(this@RegisterActivity, "Pelase Enter Password", Toast.LENGTH_LONG).show()
+
+        } else {
+            mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener { task ->
+                if (task.isSuccessful) {
+                    firebaseUserID = mAuth.currentUser!!.uid
+                    refUsers = FirebaseDatabase.getInstance().reference.child("users")
+                        .child(firebaseUserID)
+
+                    val userHashMap=HashMap<String,Any>()
+                    userHashMap["uid"]=firebaseUserID
+                    userHashMap["username"]=username
+                    userHashMap["profile"]="https://firebasestorage.googleapis.com/v0/b/messenger-7419a.appspot.com/o/profile.png?alt=media&token=3f352aa0-e40b-4e78-97a6-8cf15730e7f7"
+                    userHashMap["cover"]="https://firebasestorage.googleapis.com/v0/b/messenger-7419a.appspot.com/o/cover.png?alt=media&token=82de0dc5-ae73-4891-aa1b-f8fd96f322c4"
+                    userHashMap["status"]="Offline"
+                    userHashMap["search"]=username.toLowerCase()
+                    userHashMap["facebook"]="https://m.facebook.com"
+                    userHashMap["instagram"]="https://m.instagram.com"
+                    userHashMap["website"]="https://m.google.com"
+
+                    refUsers.updateChildren(userHashMap).addOnCompleteListener { 
+                        task ->
+                        if (task.isSuccessful){
+                            val intent=Intent(this@RegisterActivity,MainActivity::class.java)
+                            intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                            startActivity(intent)
+                            finish()
+                        }
+                    }
+
+
+
+                } else {
+                    Toast.makeText(
+                        this@RegisterActivity,
+                        "Error Message :" + task.exception!!.message.toString(),
+                        Toast.LENGTH_LONG
+                    ).show()
+                }
+            }
+        }
+
     }
 }
