38a0b0a9384bc12ca83219b45c4c87b85a31c2ed
Sameera Dissanayaka <sameerad177@gmail.com>
2020-08-03 00:06:02 +0530

    added get profile picture to main activity

--- app/src/main/java/com/example/messengerapp/MainActivity.kt
+++ app/src/main/java/com/example/messengerapp/MainActivity.kt
@@ -15,32 +15,63 @@ import androidx.viewpager.widget.ViewPager
 import com.example.messengerapp.Fragments.ChatFragment
 import com.example.messengerapp.Fragments.SearchFragment
 import com.example.messengerapp.Fragments.SettingsFragment
+import com.example.messengerapp.ModelClasses.Users
 import com.google.android.material.tabs.TabLayout
 import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.database.*
+import com.squareup.picasso.Picasso
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+    var refUsers: DatabaseReference? = null
+    var firebaseUser: FirebaseUser? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar_main))
 
-        val toolbar:androidx.appcompat.widget.Toolbar=findViewById(R.id.toolbar_main)
+
+        firebaseUser = FirebaseAuth.getInstance().currentUser
+        refUsers = FirebaseDatabase.getInstance().reference.child("Users").child(firebaseUser!!.uid)
+
+        val toolbar: androidx.appcompat.widget.Toolbar = findViewById(R.id.toolbar_main)
         setSupportActionBar(toolbar)
-        supportActionBar!!.title=""
+        supportActionBar!!.title = ""
 
-        val tabLayout:TabLayout=findViewById(R.id.tab_layout)
-        val viewPager:ViewPager=findViewById(R.id.view_pager)
+        val tabLayout: TabLayout = findViewById(R.id.tab_layout)
+        val viewPager: ViewPager = findViewById(R.id.view_pager)
 
-        val viewPagerAdapter =ViewPagerAdapter(supportFragmentManager)
+        val viewPagerAdapter = ViewPagerAdapter(supportFragmentManager)
 
-        viewPagerAdapter.addFragments(ChatFragment(),"Chats")
-        viewPagerAdapter.addFragments(SearchFragment(),"Search")
-        viewPagerAdapter.addFragments(SettingsFragment(),"Settings")
+        viewPagerAdapter.addFragments(ChatFragment(), "Chats")
+        viewPagerAdapter.addFragments(SearchFragment(), "Search")
+        viewPagerAdapter.addFragments(SettingsFragment(), "Settings")
 
-        viewPager.adapter=viewPagerAdapter
+        viewPager.adapter = viewPagerAdapter
         tabLayout.setupWithViewPager(viewPager)
 
+        //Display username and profile picture
+        refUsers!!.addValueEventListener(object : ValueEventListener {
+            override fun onDataChange(p0: DataSnapshot) {
+                if (p0.exists()) {
+                    val user: Users? = p0.getValue(Users::class.java)
+                    Picasso.get().load(user?.getProfile()).into(profile_image)
+
+                    user_name.text = user!!.getUserName()
+                }
+            }
+
+            override fun onCancelled(p0: DatabaseError) {
+
+            }
+        }
+
+
+        )
     }
 
     override fun onCreateOptionsMenu(menu: Menu): Boolean {
@@ -56,7 +87,7 @@ class MainActivity : AppCompatActivity() {
         when (item.itemId) {
             R.id.action_logout -> {
                 FirebaseAuth.getInstance().signOut()
-                val intent= Intent(this@MainActivity,WelcomeActivity::class.java)
+                val intent = Intent(this@MainActivity, WelcomeActivity::class.java)
                 intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
                 startActivity(intent)
                 finish()
@@ -67,14 +98,14 @@ class MainActivity : AppCompatActivity() {
         return false
     }
 
-    internal class ViewPagerAdapter(fragmentManager: FragmentManager):
-        FragmentPagerAdapter(fragmentManager)
-    {
-        private val fragments:ArrayList<Fragment>
-        private val titles:ArrayList<String>
+    internal class ViewPagerAdapter(fragmentManager: FragmentManager) :
+        FragmentPagerAdapter(fragmentManager) {
+        private val fragments: ArrayList<Fragment>
+        private val titles: ArrayList<String>
+
         init {
-            fragments= ArrayList<Fragment>()
-            titles=ArrayList<String>()
+            fragments = ArrayList<Fragment>()
+            titles = ArrayList<String>()
         }
 
 
@@ -85,12 +116,12 @@ class MainActivity : AppCompatActivity() {
         override fun getCount(): Int {
             return fragments.size
         }
-fun addFragments(fragment: Fragment,title:String)
-{
-    fragments.add(fragment)
-    titles.add(title)
 
-}
+        fun addFragments(fragment: Fragment, title: String) {
+            fragments.add(fragment)
+            titles.add(title)
+
+        }
 
         override fun getPageTitle(i: Int): CharSequence? {
             return titles[i]
